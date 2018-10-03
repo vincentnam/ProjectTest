6c49eb2e29531aa33c926d0075495d77de462d8e
vincent-nam <vincent-nam.dang@univ-tlse3.fr>
2018-10-03 09:05:12 +0200

    AP + print coucou

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
-
+#include <WiFi.h>
 void setup()
 {
     Serial.begin(115200);
+    WiFi.softAP("Coucou","Coucoumdp",2,0,1);
 }
 
 void loop()
 {
-
+    Serial.println("Coucou");
+    delay(1000);
+    
 
 }
